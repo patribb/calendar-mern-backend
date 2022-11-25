50d4709dfcbc945f25e7c18bb56426d72bee34fa
Patricia Garcia <martinez171214@gmail.com>
2022-11-25 11:42:50 +0100

    backend 1.2

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -9,7 +9,10 @@ export const createUser = async (req, res) => {
     user = new User(req.body);
     await user.save();
     const token = await generateJWT(user.id, user.name);
-    return res.status(201).json({user, token});
+    return res.status(201).json({ ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token});
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: 'Upsss, algo salió mal en el servidor.' })
@@ -24,7 +27,7 @@ export const loginUser = async (req, res) => {
     const requestPassword = await user.comparePassword(password);
     if (!requestPassword) return res.status(403).json({ error: 'Las credenciales no son correctas.' });
     const token = await generateJWT(user.id, user.name);
-    return res.json({  uid: user.id, name: user.name,token });
+    return res.json({  uid: user.id, name: user.name ,token });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: 'Upsss, algo salió mal en el servidor.' })
